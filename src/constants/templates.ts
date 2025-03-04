c50e65a761890b003282db2921ed41392a0ddfb8
adrian andrei schneider <schneider.adrian.andrei@gmail.com>
2025-03-04 21:22:37 +0200

    first commit

---
+++ src/constants/templates.ts
@@ -0,0 +1,123 @@
+
+
+ export const templates = [
+    {
+        id: "blank", 
+        label: "Blank Document", 
+        imageUrl: "/blank-document.png"
+    },
+    {
+        id: "software-proposal", 
+        label: "Software development proposal", 
+        imageUrl: "/software-proposal.png",
+        initialContent: `
+        <h1>Software Development Proposal </h1>
+        <h2>Project Overview</h2>
+        <p>Brief descrition of the proposed software development project.<p>
+
+        <h2>Scope of Work</h2>
+        <p>Detailed breakdown of project deliverables and requirements.<p>
+
+        <h2>Timeline</h2>
+        <p>Project milestones and delivery schedule.</p>
+
+        <h2>Budget</h2>
+        <p>Coast breakdown and payment terms.</p>
+        `
+    },
+    {
+        id: "project-proposal", 
+        label: "Project Proposal", 
+        imageUrl: "/project-proposal.png",
+        initialContent: `
+        <h1>Project Proposal </h1>
+        <h2>Executive Summary</h2>
+        <p>Brief descrition of the project proposal.<p>
+
+        <h2>Project Goals/h2>
+        <p>Key objectives and expected outcomes.<p>
+
+        <h2>Implementation Plan</h2>
+        <p>Strategy and methodology for project execution.</p>
+
+        <h2>Resources Required</h2>
+        <p>Team, equipment, and budget requirements.</p>
+        `
+    },
+    {
+        id: "business-letter", 
+        label: "Business letter", 
+        imageUrl: "/business-letter.png",
+        initialContent: `
+        <h1>Software Development Proposal </h1>
+        <h2>Project Overview</h2>
+        <p>Brief descrition of the proposed software development project.<p>
+
+        <h2>Scope of Work</h2>
+        <p>Detailed breakdown of project deliverables and requirements.<p>
+
+        <h2>Timeline</h2>
+        <p>Project milestones and delivery schedule.</p>
+
+        <h2>Budget</h2>
+        <p>Coast breakdown and payment terms.</p>
+        `
+    },
+    {
+        id: "resume", 
+        label: "Resume", 
+        imageUrl: "/resume.png",
+        initialContent: `
+        <h1>Software Development Proposal </h1>
+        <h2>Project Overview</h2>
+        <p>Brief descrition of the proposed software development project.<p>
+
+        <h2>Scope of Work</h2>
+        <p>Detailed breakdown of project deliverables and requirements.<p>
+
+        <h2>Timeline</h2>
+        <p>Project milestones and delivery schedule.</p>
+
+        <h2>Budget</h2>
+        <p>Coast breakdown and payment terms.</p>
+        `
+    },
+    {
+        id: "cover-letter", 
+        label: "Cover letter", 
+        imageUrl: "/cover-letter.png",
+        initialContent: `
+        <h1>Software Development Proposal </h1>
+        <h2>Project Overview</h2>
+        <p>Brief descrition of the proposed software development project.<p>
+
+        <h2>Scope of Work</h2>
+        <p>Detailed breakdown of project deliverables and requirements.<p>
+
+        <h2>Timeline</h2>
+        <p>Project milestones and delivery schedule.</p>
+
+        <h2>Budget</h2>
+        <p>Coast breakdown and payment terms.</p>
+        `
+    },
+    {
+        id: "letter", 
+        label: "Letter", 
+        imageUrl: "/letter.png",
+        initialContent: `
+        <h1>Software Development Proposal </h1>
+        <h2>Project Overview</h2>
+        <p>Brief descrition of the proposed software development project.<p>
+
+        <h2>Scope of Work</h2>
+        <p>Detailed breakdown of project deliverables and requirements.<p>
+
+        <h2>Timeline</h2>
+        <p>Project milestones and delivery schedule.</p>
+
+        <h2>Budget</h2>
+        <p>Coast breakdown and payment terms.</p>
+        `
+    },
+];
